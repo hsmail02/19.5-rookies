34425216e55343ce838f13b089670524ae33a171
Hyunsu Go <hsmail02@naver.com>
2021-09-03 10:33:49 +0900

    remove misplaced assignment0 dir

--- assignment0/survey/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-from survey import views
-
-urlpatterns = [
-    path('results/', views.get_survey_results, name="get_surveys"),
-    path('results/<survey_id>/', views.get_survey, name="get_survey"),
-]
